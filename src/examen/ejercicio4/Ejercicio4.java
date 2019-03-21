1745315c7cce33f5faaa8504f257d1b6dd9f8549
Cristinamf <krismf98@gmail.com>
2019-03-21 14:19:30 +0100

    solucion final

--- src/examen/ejercicio4/Ejercicio4.java
+++ src/examen/ejercicio4/Ejercicio4.java
@@ -13,9 +13,15 @@ public class Ejercicio4 {
 		do{
 			System.out.println("Entrada: ");
 			n=sc.nextLine();
+			String buscar="buscar:";
+			String[] divisionAgenda = n.split("-");
+			String nombre = divisionAgenda[0]; // 123
+			String telefono = divisionAgenda[1]; // 654321
 			if(n!="fin") {
 				if(!agenda.contains(n))
 					agenda.add(n);
+				else if(agenda.contains(nombre))
+					agenda.set(divisionAgenda.length+1,telefono);
 			}
 				
 			
